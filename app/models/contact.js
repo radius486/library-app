aeb6f0df7c30744639256307ce236e5851cfa6bf
Rodion Grishaev <radius486@gmail.com>
2016-10-12 03:52:29 +0300

    Contact in model

--- app/models/contact.js
+++ app/models/contact.js
@@ -2,5 +2,28 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+
+  isEmailValid: Ember.computed.match('email', /^.+@.+\..+$/),
+  isMessageValid: Ember.computed.gte('message.length', 5),
+  isValid: Ember.computed.and('isEmailValid', 'isMessageValid'),
+  isDisabled: Ember.computed.not('isValid'),
+
+  emailFieldClass: Ember.computed('isEmailValid', function() {
+    if (this.get('isEmailValid')) {
+      return 'form-group has-success has-feedback';
+    }
+    else {
+      return 'form-group has-feedback';
+    }
+  }),
+
+  messageFieldClass: Ember.computed('isMessageValid', function() {
+    if (this.get('isMessageValid')) {
+      return 'form-group has-success has-feedback';
+    }
+    else {
+      return 'form-group has-feedback';
+    }
+  }),
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+    sendMessage(newMessage) {
+      const data = `Email: ${this.controller.get('model.email')}, Message: ${this.controller.get('model.message')}`;
+      //alert(data);
+
+      newMessage.save().then(() => {
+        this.controller.set('model.email', '');
+        this.controller.set('model.message', '');
+        this.controller.set('model.responseMessage', `We got your message and we’ll get in touch soon. ${data}`);
+      });
+
+    },
+
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
